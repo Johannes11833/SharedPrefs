ca96333f3d121c2bb763456205865ee50670c134
Johannes Gundlach <johannes11833@scc-wkit-clxa-23-197.scc.kit.edu>
2018-07-19 11:51:23 +0200

    init

---
+++ app/src/main/java/com/example/johannes11833/sharedpref/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.johannes11833.sharedpref;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setValue();
+
+        getValue();
+    }
+
+
+    void setValue(){
+        SharedPreferences.Editor editor = getSharedPreferences("abc", MODE_PRIVATE).edit();
+        editor.putString("name", "Elena");
+        editor.putInt("idName", 12);
+        editor.apply();
+
+    }
+
+
+
+    void getValue(){
+        SharedPreferences prefs = getSharedPreferences("abc", MODE_PRIVATE);
+        String restoredText = prefs.getString("name", null);
+        if (restoredText != null) {
+
+
+            Log.d("sharedPref_string",restoredText);
+        }else{
+            Log.d("sharedPref_string","not found");
+
+        }
+
+    }
+}
